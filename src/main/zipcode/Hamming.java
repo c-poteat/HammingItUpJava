0138a615fd53c793ef2f4582a83ac13eedec61ae
Carnell <92267723+c-poteat@users.noreply.github.com>
2021-12-15 22:48:33 -0500

    Updated and completed lab

--- src/main/zipcode/Hamming.java
+++ src/main/zipcode/Hamming.java
@@ -1,10 +1,31 @@
 package zipcode;
 
+import org.junit.rules.ExpectedException;
+
 public class Hamming {
+    private String s;
+    private String s1;
+
     public Hamming(String s, String s1) {
+        this.s = s;
+        this.s1 = s1;
+        if (s.length() != s1.length()) {  // If strings are not equal
+            // Throw exception
+            throw new IllegalArgumentException("leftStrand and rightStrand must be of equal length.");
+        }
     }
 
-    public int getHammingDistance() {
-        return -1;
+    public int getHammingDistance(){
+        // Declare variables and place into char array
+        int hammingDistance = 0;
+        int count = 0;
+        for (char c : s.toCharArray()) {
+            if (c != s1.toCharArray()[count]) {
+                hammingDistance++;
+            }
+            count++;
+        }
+        return hammingDistance;
     }
+
 }
